60bbf8a57a7b137bbc503acf90c8f0e78f0bb1f5
Lucas Cordella <100384378+lucascordella@users.noreply.github.com>
2023-03-31 16:35:02 -0300

    Add files via upload

---
+++ problema_menor_de_tres/problema_menor_de_tres/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int N1, N2, N3, menor;
+
+            Console.Write("Primeiro valor: ");
+            N1 = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            N2 = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            N3 = int.Parse(Console.ReadLine());
+
+            menor = N1;
+            if (menor > N2)
+            {
+                menor = N2;
+            }
+            if (menor > N3)
+            {
+                menor = N3;
+            }
+
+            Console.Write("MENOR = " +  menor);
+        }
+    }
+}
