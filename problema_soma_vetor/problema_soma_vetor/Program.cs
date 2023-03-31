926c58d1b18acf8f84279fe352d5c2fd6ac1227f
Lucas Cordella <100384378+lucascordella@users.noreply.github.com>
2023-03-31 16:37:03 -0300

    Add files via upload

---
+++ problema_soma_vetor/problema_soma_vetor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double soma, media;
+            int N, i;
+
+            Console.Write("Quantos números você vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            for(i = 0; i < N; i++)
+            {
+                Console.Write("Digite um número: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.WriteLine();
+            Console.Write("VALORES = ");
+            for (i = 0; i < N; i++)
+            {
+                Console.Write(vet[i].ToString("F1", CI) + "  ");
+            }
+
+            soma = 0;
+            for (i = 0; i < N; i++)
+            {
+                soma = soma + vet[i];
+            }
+
+            media = soma / N;
+
+            Console.WriteLine();
+            Console.WriteLine("SOMA = " + soma.ToString("F2", CI));
+            Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+        }
+    }
+}
