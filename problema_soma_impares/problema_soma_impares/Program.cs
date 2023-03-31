446560870ef7269bc5c673aa73f8e1b85af43371
Lucas Cordella <100384378+lucascordella@users.noreply.github.com>
2023-03-31 16:36:29 -0300

    Add files via upload

---
+++ problema_soma_impares/problema_soma_impares/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int X, Y, i, soma, troca;
+
+            Console.WriteLine("Digite dois números: ");
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+
+            if (X > Y)
+            {
+                troca = X;
+                X = Y;
+                Y = troca;
+            }
+
+            soma = 0;
+            for (i = X + 1; i < Y; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine("SOMA DOS IMPARES = " + soma);
+
+        }
+    }
+}
