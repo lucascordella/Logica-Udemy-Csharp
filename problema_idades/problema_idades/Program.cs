233075f99a6b3bda53495ee49d18b605d49580cd
Lucas Cordella <100384378+lucascordella@users.noreply.github.com>
2023-03-31 16:33:05 -0300

    Add files via upload

---
+++ problema_idades/problema_idades/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture; 
+
+            int idade1, idade2;
+            string nome1, nome2;
+            double media;
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade1 = int.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade2 = int.Parse(Console.ReadLine(), CI);
+
+            media = (idade1 + idade2) / 2.0;
+            Console.WriteLine("A idade media de " + nome1 + " e " + nome2 + " é de " + media.ToString("F1", CI) + " anos");
+        }
+    }
+}
