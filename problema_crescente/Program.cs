cf9ea91589dfe6626657245573695f6aeba18a63
Lucas Cordella <lucas.cordella0804@gmail.com>
2023-03-31 16:28:32 -0300

    Adicionar arquivos de projeto.

---
+++ problema_crescente/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int X, Y;
+
+            Console.WriteLine("Digite dois números:");
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+
+            while (X != Y)
+            {
+                if (X < Y)
+                {
+                    Console.WriteLine("CRESCENTE!");
+                }
+                else
+                {
+                    Console.WriteLine("DESCRESCENTE!");
+                }
+
+                Console.WriteLine("Digite outros dois números:");
+                X = int.Parse(Console.ReadLine());
+                Y = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
